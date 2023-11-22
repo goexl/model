6d7c466b1d74925c7d9109360f7389e851b87f21
storezhang <storezhang@gmail.com>
2023-11-22 22:03:16 +0800

    refactor(模型): 增加id为字符串的返回

--- base.go
+++ base.go
@@ -1,19 +1,20 @@
 package model
 
 import (
+	"strconv"
 	"time"
 )
 
 // Base 基础数据库模型
 type Base struct {
 	// 编号
-	Id int64 `xorm:"pk notnull unique index('idx_id') default(0)" json:"id,string,omitempty"`
+	Id int64 `xorm:"pk notnull default(0)" json:"id,string,omitempty"`
 	// 创建时间
 	Created time.Time `xorm:"created notnull default('2022-12-23 09:55:52')" json:"created,omitempty"`
 	// 最后更新时间
 	Updated time.Time `xorm:"updated notnull default('2022-12-23 09:55:52')" json:"updated,omitempty"`
 }
 
-func (b *Base) Identify() int64 {
-	return b.Id
+func (b *Base) Identify() string {
+	return strconv.FormatInt(b.Id, 64)
 }
