21e01955d97523055f859d0d85f9345ea3819f6c
storezhang <storezhang@gmail.com>
2024-05-18 09:40:50 +0800

    feat(模型): 时间默认值为CURRENT_TIMESTAMP

--- delete.go
+++ delete.go
@@ -9,5 +9,5 @@ type Delete struct {
 	// 删除时间
 	// 用于实现软删除
 	// nolint:lll
-	Deleted time.Time `xorm:"deleted notnull default('2022-12-23 09:55:52') comment('删除时间，通过本字段实现实现软删除功能，使用删除功能时自动设置本时间')" json:"deleted,omitempty"`
+	Deleted time.Time `xorm:"deleted notnull default(CURRENT_TIMESTAMP) comment('删除时间，通过本字段实现实现软删除功能，使用删除功能时自动设置本时间')" json:"deleted,omitempty"`
 }
--- update.go
+++ update.go
@@ -8,5 +8,5 @@ import (
 type Update struct {
 	// 最后更新时间
 	// nolint:lll
-	Updated time.Time `xorm:"updated notnull default('2022-12-23 09:55:52')  comment('最后更新时间，每次更新时自动修改')" json:"updated,omitempty"`
+	Updated time.Time `xorm:"updated notnull default(CURRENT_TIMESTAMP) comment('最后更新时间，每次更新时自动修改')" json:"updated,omitempty"`
 }
